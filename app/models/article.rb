f089ac37a28f9867d5e38c95e75224ffe11c2682
akushniruk <akushniruk@heliostech.fr>
2020-09-06 20:04:59 +0300

    Add ont-to-many association between users and articles from back end

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+  belongs_to :user
   validates :title, presence: true, length: { minimum: 6, maximum: 100 }
   validates :description, presence: true, length: { minimum: 10, maximum: 255 }
   validates :img_url, presence: true, length: { minimum: 20, maximum: 255 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_06_111215) do
+ActiveRecord::Schema.define(version: 2020_09_06_165305) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_09_06_111215) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string "img_url"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
